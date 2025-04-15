caa100504dfd50f46a56c30380bb24c97dea76cf
valeria <valeria@valeria.valeria>
2025-04-15 00:26:20 -0300

    agregue la ejecucion interactiva desde la consola que se me olvido agregar

--- src/palindrome.py
+++ src/palindrome.py
@@ -9,3 +9,13 @@ def is_palindrome(text):
     # y este es el Usa las funciones auxiliares para verificar si es palíndromo
     cleaned = limpiar_texto(text)
     return comparar_textos(cleaned, cleaned[::-1])
+if __name__ == "__main__":
+    try:
+        while True:
+            entrada = input("Ingrese una palabra o frase: ")
+            if is_palindrome(entrada):
+                print(f'"{entrada}" es un palíndromo')
+            else:
+                print(f'"{entrada}" no es un palíndromo')
+    except KeyboardInterrupt:
+        print("\nPrograma finalizado.")
