6784f389bc6d0e08760491028fde92df1fb88b64
valeria <valeria@valeria.valeria>
2025-04-14 23:47:03 -0300

    Implementar función comparar_textos para comparar caracteres

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,7 +1,11 @@
 def limpiar_texto(texto):
     return ''.join(c.lower() for c in texto if c.isalnum())
+# este va a elimina espacios, signos y pasar todo a minúsculas
 
+def comparar_textos(original, invertido):
+    return original == invertido
+# este es el q Compara si el texto limpio es igual a su reverso
 def is_palindrome(text):
-    # Esta funcion en es la que eliminara los espacios y pasara todo a minúsculas
-    cleaned = ''.join(c.lower() for c in text if c.isalnum())
-    return cleaned == cleaned[::-1]
+    # y este es el Usa las funciones auxiliares para verificar si es palíndromo
+    cleaned = limpiar_texto(text)
+    return comparar_textos(cleaned, cleaned[::-1])
