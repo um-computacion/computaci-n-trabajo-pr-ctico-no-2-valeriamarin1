e571731205ced8d31011e4c0cc5db8188a555fe8
valeria <valeria@valeria.valeria>
2025-04-14 23:18:13 -0300

    Implementar función limpiar_texto y actualizar is_palindrome

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,3 +1,6 @@
+def limpiar_texto(texto):
+    return ''.join(c.lower() for c in texto if c.isalnum())
+
 def is_palindrome(text):
     # Esta funcion en es la que eliminara los espacios y pasara todo a minúsculas
     cleaned = ''.join(c.lower() for c in text if c.isalnum())
